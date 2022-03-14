59398cb49a61436c33b34c64d660e9076559a056
StephanieCobble <cobble.stephanie@gmail.com>
2022-03-14 13:48:45 -0700

    created admin account and saved two new cars and successfully tested in Postman

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .Serializers import CarSerializer
+from .models import Car
+
 
 @api_view(['GET'])
 def cars_list(request):
+    cars = Car.objects.all()
+    serializer = CarSerializer(cars, many=True)
 
-    return Response('ok')
+    return Response(serializer.data)
